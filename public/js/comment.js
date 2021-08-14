aa835a610d098e6286c82331043b9a9593920404
Zahra Seyedi <za.seyedi@gmail.com>
2021-08-14 07:49:25 -0700

    creat comment

---
+++ public/js/comment.js
@@ -0,0 +1,24 @@
+const createCommentHandler = async (event) => {
+  event.preventDefault();
+  const currentURL = document.location;
+  const urlArr = document.location.href.split("/");
+  const post_id = urlArr[urlArr.length - 1];
+  const body = document.querySelector("#comment").value.trim();
+
+  if (body && post_id) {
+    const response = await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ body, post_id }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      document.location.replace(currentURL);
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+  .querySelector(".create-comment")
+  .addEventListener("submit", createCommentHandler);
