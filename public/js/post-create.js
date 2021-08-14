e4369cc11a9fd9fabaed278ba48684c4a38e8888
Zahra Seyedi <za.seyedi@gmail.com>
2021-08-14 07:47:46 -0700

    creat post

---
+++ public/js/post-create.js
@@ -0,0 +1,23 @@
+const createPostHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector("#title").value.trim();
+  const content = document.querySelector("#content").value.trim();
+
+  if (title && content) {
+    const response = await fetch("/api/posts", {
+      method: "POST",
+      body: JSON.stringify({ title, content }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+  .querySelector(".create-post")
+  .addEventListener("submit", createPostHandler);
