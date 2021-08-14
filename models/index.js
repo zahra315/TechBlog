a5d69daf3c809be068359b1e7a240c80afb27e44
Zahra Seyedi <za.seyedi@gmail.com>
2021-08-14 07:27:46 -0700

    CASCADE

--- models/index.js
+++ models/index.js
@@ -1,32 +1,32 @@
 const User = require("./User");
 const Post = require("./Post");
-const Comment = require("./Comment.js");
+const Comment = require("./Comment");
 
 User.hasMany(Post, {
   foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
 });
 
 User.hasMany(Comment, {
   foreignKey: "user_id",
-  onDelete: "cascade",
+  onDelete: "CASCADE",
 });
 
-Post.belongTo(User, {
+Comment.belongsTo(User, {
   foreignKey: "user_id",
-  onDelete: "cascade",
 });
 
-Post.hasMany(Comment, {
-  foreignKey: "user_id",
-  onDelete: "cascade",
-});
-
-Comment.belongTo(User, {
-  foreignKey: "user_id",
-  onDelete: "cascade",
-});
-
-Comment.belongTo(Post, {
-  foreignKey: "user_id",
-  onDelete: "cascade",
-});
+module.exports = { User, Post, Comment };
