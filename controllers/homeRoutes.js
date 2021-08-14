5c604da7cc4858f56efb937497c9ae2f91bc169e
Zahra Seyedi <za.seyedi@gmail.com>
2021-08-14 07:24:26 -0700

    creat comment on post

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,19 +1,18 @@
 const router = require("express").Router();
-const { Post, User } = require("../models");
+const { User, Post, Comment } = require("../models");
 const withAuth = require("../utils/auth");
 
+// index route shows all existing posts on the blog
 router.get("/", async (req, res) => {
   try {
-    // Get all post and JOIN with user data
     const postData = await Post.findAll({
       include: [
         {
           model: User,
-          attributes: ["name"],
+          attributes: ["username"],
         },
       ],
     });
-
     // Serialize data so the template can read it
     const posts = postData.map((post) => post.get({ plain: true }));
 
@@ -27,21 +26,87 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/post/:id", async (req, res) => {
+// GET a specific Post by id
+router.get("/post/:id", withAuth, async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id, {
       include: [
         {
           model: User,
-          attributes: ["name"],
+          attributes: ["username"],
         },
       ],
     });
 
     const post = postData.get({ plain: true });
 
-    res.render("post", {
-      ...post,
+    const commentData = await Comment.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+      where: {
+        post_id: req.params.id,
+      },
+    });
+    const comments = commentData.map((comment) => comment.get({ plain: true }));
+
+    if (req.session.user_id === post.user_id) {
+      // if the post belongs to the user, then the user can update or delete it
+      res.render("post-edit", {
+        ...post,
+        logged_in: req.session.logged_in,
+      });
+    } else {
+      // if the post does not belong to logged in user, then the user can add a comment
+      res.render("post", {
+        ...post,
+        comments,
+        logged_in: req.session.logged_in,
+      });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Render the logged in user's dashboard
+router.get("/dashboard", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+      where: {
+        user_id: req.session.user_id,
+      },
+    });
+    if (postData.length > 0) {
+      const posts = postData.map((post) => post.get({ plain: true }));
+
+      res.render("dashboard", {
+        posts,
+        logged_in: req.session.logged_in,
+      });
+    } else {
+      res.render("dashboard", {
+        logged_in: req.session.logged_in,
+      });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get the page where a logged in user can create a post
+router.get("/post-create", withAuth, async (req, res) => {
+  try {
+    res.render("post-create", {
       logged_in: req.session.logged_in,
     });
   } catch (err) {
@@ -49,34 +114,19 @@ router.get("/post/:id", async (req, res) => {
   }
 });
 
-// Use withAuth middleware to prevent access to route
-router.get("/profile", withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ["password"] },
-      include: [{ model: Post }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render("profile", {
-      ...user,
-      logged_in: true,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 router.get("/login", (req, res) => {
-  // If the user is already logged in, redirect the request to another route
+  // If the user is already logged in, redirect the request to homepage
   if (req.session.logged_in) {
-    res.redirect("/profile");
+    res.redirect("/");
     return;
   }
 
   res.render("login");
+});
+
+// Route for users who need the sign-up form
+router.get("/signup", (req, res) => {
+  res.render("signup");
 });
 
 module.exports = router;
